c56ec42e0cf1e93f53075a67983665fdee1570bd
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-12 06:04:31 -0700

    task 3

--- 0x00-hello_world/3-main.c
+++ 0x00-hello_world/3-main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* 
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("cisfun");
+	FILE *out=fopen("0-main.c","w");
+	fputs("Hello File",out);
+	fclose(out);
 	return (0);
 }
