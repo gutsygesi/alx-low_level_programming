fb8f4abdd5634b4e00357ac5c2ead4dc485bc55b
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-11 21:31:03 -0700

    tsk project 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-
+/**
+ * Main entry point
+ *
+ * Return: Aways 0 (Successs)
+ */
 int main(void)
 {
        	int intType;
 	float floatType;
        	double doubleType;
        	char charType;
+	int longType;
 
-	 printf("Size of int: %zu bytes\n", sizeof(intType));
-	 printf("Size of float: %zu bytes\n", sizeof(floatType));
-	 printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	 printf("Size of char: %zu byte\n", sizeof(charType));
+	 printf("Size of a char: %zu bytes\n", sizeof(charType));
+	 printf("Size of a int: %zu bytes\n", sizeof(intType));
+	 printf("Size of a long int: %zu bytes\n", sizeof(longType));
+	 printf("Size of a long long int: %zu bytes\n", sizeof(doubleType));
+	 printf("Size of a float: %zu byte\n", sizeof(charType));
 	                      
-	 return 0;
- }
+	 return(0);
+}
