7dc7d1e1a21e3414bfef77011b28bef02467acd9
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-11 22:03:28 -0700

    Project tsk 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-       	int intType;
+	int intType;
 	float floatType;
-       	double llType;
-       	char charType;
+	double llType;
+	char charType;
 	int longType;
 
-	 printf("Size of a char: %zu bytes\n", sizeof(charType));
-	 printf("Size of a int: %zu bytes\n", sizeof(intType));
-	 printf("Size of a long int: %zu bytes\n", sizeof(longType));
-	 printf("Size of a long long int: %zu bytes\n", sizeof(llType));
-	 printf("Size of a float: %zu bytes\n", sizeof(floatType));
+       	printf("Size of a char: %zu bytes\n", sizeof(charType));
+	printf("Size of a int: %zu bytes\n", sizeof(intType));
+	printf("Size of a long int: %zu bytes\n", sizeof(longType));
+	printf("Size of a long long int: %zu bytes\n", sizeof(llType));
+	printf("Size of a float: %zu bytes\n", sizeof(floatType));
 	                      
-	 return(0);
+	return(0);
 }
