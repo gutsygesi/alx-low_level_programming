5f0d2f4fb980ebea1bbc190ec863394fb6641437
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-11 21:51:41 -0700

    project task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,14 @@ int main(void)
 {
        	int intType;
 	float floatType;
-       	double doubleType;
+       	double llType;
        	char charType;
 	int longType;
 
 	 printf("Size of a char: %zu bytes\n", sizeof(charType));
 	 printf("Size of a int: %zu bytes\n", sizeof(intType));
 	 printf("Size of a long int: %zu bytes\n", sizeof(longType));
-	 printf("Size of a long long int: %zu bytes\n", sizeof(doubleType));
+	 printf("Size of a long long int: %zu bytes\n", sizeof(llType));
 	 printf("Size of a float: %zu bytes\n", sizeof(floatType));
 	                      
 	 return(0);
