922cde199d7195e2fbc3e128113eeb29215d2624
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-11 19:04:05 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main -Entry point
  *
  * Return: Always 0 (Success)
  */
-int main()
-{	
-	char text[]= "Programming is like building a multilingual puzzle";
-	puts(text);
+int main(void)
+{
+	char Textstr [] = "Progrqamming is like building a multilingual puzzle";
+	puts(Textstr);
 	return (0);
 }
