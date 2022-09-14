3b6bca5ec5eade1c16624544b04e3256d8aac919
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-14 16:46:53 -0700

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	putchar('\n');
+
+	}
+	return (0);
+
+}
