4b8c58a2ffad6e61226521a10969cd67ee943fe1
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-14 20:34:02 -0700

    correction project

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+		_putchar('\n');
+	}
 }
