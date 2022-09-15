a6ca0a3c9e168ede944744e8c76a6c4f762e31d2
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-14 18:51:53 -0700

    project task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - check the code
  *
@@ -6,14 +6,6 @@
  */
 int main(void)
 {
-	int ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	putchar(ch);
-	putchar('\n');
-
-	}
+	print_alphabet();
 	return (0);
-
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,11 +7,31 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void print_alphabet(void)
 {
 	int ch;
-	for (ch= ‘a’; ch<= ‘z’; ch++)
-	putchar(ch);
-	putchar (‘\n’) ;
-	getch();
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	_putchar(ch);
+	_putchar('\n');
+	}
+	
+}
+void print_alphabet_x10(void)
+{
+	int n, co;
+	co = 0;
+	while (co < 10)
+	{
+	for (n = 'a'; n <= 'z'; n++)
+	{
+	_putchar(n);
+	}
+	co++;
+	_putchar('\n');
+	}
 }
