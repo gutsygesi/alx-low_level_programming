a62a89e08e2351e4157898a50a41a3c6ce01abc3
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-20 02:10:59 -0700

    PROJECT TASK 7 CORRECTION

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,25 +10,23 @@ void puts_half(char *str)
 	int len, n, i;
 
 	len = 0;
-	
 	while (str[len] != '\0')
 	{
-	len++;
+		len++;
 	}
 
 	if (len % 2 == 0)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
-	{
-																	_putchar(str[i]);												}
+		{
+			_putchar(str[i]);												}
 	}
-	else if (len % 2)													{
+	else if (len % 2)
+	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
-	{
-	
-		_putchar(str[n + 1]);
+		{
+			_putchar(str[n + 1]);
+		}
 	}
-	
-	}
-		_putchar('\n');
+	_putchar('\n');
 }
