4054aa53715bbf8f52d7ef5e9c31971e2159d327
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-20 07:48:26 -0700

    project tsk 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and also in uppercase
+ * followed by a new line of output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
