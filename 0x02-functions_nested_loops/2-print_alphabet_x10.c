66d45b3aae352ff0a7cf159eead522127838260e
bufumoh Joshua Gesitari <gutsygesi1@gmil.com>
2022-09-21 11:32:20 -0700

    project tsk 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
 #include "main.h"
 /**
-* main - check the code.
-*
-* Return: Always 0.
-*/
-int main(void)
+ * print_alphabet_x10 - prints the alphabet, in lowercase, followed by a new line
+ */
+void print_alphabet_x10(void)
 {
-	 print_alphabet_x10();
-	return (0);
+	char ch;
+	int i;
+
+	i = 0;
+	while (i <= 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	}
+	_putchar('\n');
+	i++;
 }
